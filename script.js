f24652d86057ba2f3e2df948b82c4c7214a04d85
Anne Ensign <anne.ensign@gmail.com>
2021-03-15 12:11:25 -0400

    Detailed notes on scripts.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-/*=============================================================
-----------CLASSES
-=============================================================*/
+/**=============================================================
+* 
+**                          CLASSES
+*
+*=============================================================*/
 
-//  Attributes for every bubble
+/**
+ * *------  BUBBLE  ------
+ * For indivudal attributes assigned to each bubble
+ */
 
 class Bubble {
     constructor (x, rate, color, scale, size) {
@@ -14,7 +19,10 @@ class Bubble {
     }
 }
 
-//  Bubble factory class
+/**
+ * *------  MULTIBUBBLE ------
+ * Bubble making factory to make 5 bubbles at a time
+ */
 
 class Multibubble {
     constructor() {
@@ -23,7 +31,7 @@ class Multibubble {
         this.render();
     }
 
-    //  Add 5 individualized bubbles, push to this.bubbles array
+    //  Function adds 5 individualized bubbles, push to this.bubbles array
 
     makeBubble() {
         let bubbleDivs = ['bubble1', 'bubble2', 'bubble3', 'bubble4', 'bubble5'];
@@ -72,15 +80,17 @@ class Multibubble {
             div.style.transform = `translateY(-100%) scale(${this.bubbles[i].scale})`;
             div.style.width = `${this.bubbles[i].size}px`;
             div.style.height = `${this.bubbles[i].size}px`;
-
         } 
-    }   
-    check(){
-        console.log(this.bubbles);  }    
+    }      
 }
 
-
-//  Extend class to add new, unique bubbles for "more bubbles" button
+/**
+ * *------  EXTRABUBBLE ------
+ * Extends the Multibubble class so the factory doesn't
+ * repeat the previous values when adding on
+ * more bubbles with the "MORE BUBBLES" button.
+ * See note at bottom of doc for details.
+ */
 
 class Extrabubble extends Multibubble {
     makeBubbleSize() {
@@ -91,14 +101,17 @@ class Extrabubble extends Multibubble {
     }
 }
 
+//*------   FIRST BUBBLES ON PAGE LOAD ------
 const myBubbles = new Multibubble;
-myBubbles.check();
 
-/*=============================================================
-----------HTML LINKS & EVENT LISTENERS
-=============================================================*/
 
-//  More bubbles button
+/**============================================================
+* 
+**              HTML LINKS & EVENT LISTENERS
+*
+*=============================================================*/
+
+//*--------  MORE BUBBLES button ------
 
 const bubbleButton = document.getElementById('more_bubbles');
 
@@ -107,23 +120,40 @@ bubbleButton.addEventListener('click', () => {
 })
 
 
-//  Reset button to clear screen
+//*------  RESET button to clear screen  ------
 
 const resetButton = document.getElementById('reset');
 
 resetButton.addEventListener('click', () => {
    let bubbleField = document.getElementsByClassName('bubble');
+   //   Iterate and remove each child
    for (let i = bubbleField.length - 1; i >= 0; i--) {
        let pop = bubbleField[i];
        document.getElementById('main').removeChild(pop);
    }
 })
 
-//  Faster button
+//*------  FASTER button ------
 
 const faster = document.getElementById('faster');
 
+    /**                 Event Listener: Iteration Notes
+     * ------------------------------------------------------------------------
+     * @param speed         Iterate through style property to find 
+     *                      "animation-duration" time (string).
+     *                       
+     * @param oldSpeed      Parse the string to find integer. Assigned here.                      
+     *      
+     *  Remove the current duration property and replace with the new one.
+     * 
+     * @param newSpeed      Takes the current duration and adds half a second.
+     * 
+     *  Will increase every click. Maximum speed set to 0.5s
+     * -------------------------------------------------------------------------
+     */
+
 faster.addEventListener('click', () => {
+    //  Declare all appended children with class "bubble"
     let allBubbles = document.getElementsByClassName('bubble');
     for (let i = 0; i < allBubbles.length; i++) {
         let speed = allBubbles[i].style.animationDuration;
@@ -138,7 +168,13 @@ faster.addEventListener('click', () => {
     }
 })
 
-//  SLOWER BUTTON
+
+/*//*------  SLOWER BUTTON ------
+ *  Works exact same as "FASTER" button
+    except that it adds time to the duration
+    instead of subtracting it.
+    Max "slowness": 20 seconds.
+ */
 
 const slower = document.getElementById('slower');
 
@@ -156,3 +192,26 @@ slower.addEventListener('click', () => {
         }
     }
 })
+
+
+
+
+
+/**
+ * !    What's up with the EXTRABUBBLE class?
+ * 
+ *      Clicking the "MORE BUBBLES" button without this extended class
+ *      made a duplicate of the previous added class instead of just adding 5 more.
+ * 
+ *      For example:
+ *      (Letters represent indivudual bubbles)
+ * 
+ *      ON LOAD [5 total]:          A B C D E
+ *      ONE CLICK [15 total]:       A B C D E   A B C D E   F G H I J
+ *      TWO CLICKS [30 total]:      A B C D E   A B C D E   F G H I J   A B C D E   F G H I J   K L M N O
+ *     
+ *      By extending the class and using "super" to copy methods,
+ *      the functon creates and pushes unique bubbles to the 
+ *      Bubble class and bubbles array.
+ * 
+ */
