f7bb40650d2378d239ce406b5b1cba8e3e71f1c9
Anne Ensign <anne.ensign@gmail.com>
2021-03-17 10:30:42 -0400

    Conditionals on new bubbles to toggle class, reset buttons update, image credits, READEME update

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # BUBBLES
 
-## DEMO:
+**Demo**
 https://eleanor-shellstrop.github.io/bubbles/
 
 This project is to demostrate OOP in JavaScript and DOM maniupulation. 
 
-Click the "MORE BUBBLES" button to add more bubbles.
+Click the buttons to change the bubbles!
+* "MORE BUBBLES" button to add more bubbles.
+* "FASTER BUBBLES" to speed up the bubbles every click.
+* "SLOWER BUBBLES" to slow down the bubbles with every click.
+* "BIGGER BUBBLES" to make them x2 larger.
+* "GOTH," "DISCO," and "FANCY" buttons to change the appearance.
+  
 Click the "RESET BUBBLES" button to clear the screen.
 
-Hat image credit to https://webstockreview.net/
+Image credits:
+* Hat & monocle image: https://webstockreview.net/
+* Disco ball image: https://www.stickpng.com/img/objects/party/disco-ball
+* Disco floor image: by Ben Hanbury, https://www.Vimeo.com/83880963
+* Rose image: https://wallpaperaccess.com/
--- script.js
+++ script.js
@@ -4,10 +4,18 @@
 *
 *=============================================================*/
 
-/**
- * *------  BUBBLE  ------
- * For indivudal attributes assigned to each bubble
- */
+/** ---------------------------------------------------------------------------
+ * *----------------------------  BUBBLE  -------------------------------------
+ * 
+ * For indivudal attributes assigned to each bubble:
+ *  @param  x       Where bubble appears across x-axis of width
+ *  @param  delay   Stagger entrance so new bubbles appear at different times
+ *  @param  rate    Rate of speed bubble moves to top
+ *  @param  color   One of four color options
+ *  @param  scale   Transform size of the bubbles toward the top of screen
+ *  @param  size    Vary the size of the bubbles  
+ *   ----------------------------------------------------------------------------    */
+
 
 class Bubble {
     constructor (x, delay, rate, color, scale, size) {
@@ -20,10 +28,11 @@ class Bubble {
     }
 }
 
-/**
- * *------  MULTIBUBBLE ------
+/** -----------------------------------------------------------------------------
+ * *---------------------------  MULTIBUBBLE ------------------------------------
+ * 
  * Bubble making factory to make 5 bubbles at a time
- */
+ *   -----------------------------------------------------------------------------*/
 
 class Multibubble {
     constructor() {
@@ -31,36 +40,23 @@ class Multibubble {
         this.makeBubble();
         this.render();
     }
-
     //  Function adds 5 individualized bubbles, push to this.bubbles array
-
     makeBubble() {
+        //  Array to iterate for 5 unique bubbles
         let bubbleDivs = ['bubble1', 'bubble2', 'bubble3', 'bubble4', 'bubble5'];
         for (let i = 0; i < bubbleDivs.length; i++) {
-
-            //  Percentage point across x-axis where bubble will appear
             let moveX = Math.floor(Math.random() * 100);
-
-            //  Stagger animation start between 0 - 5 seconds
             let wait = Math.floor(Math.random() * 5);
-
-            //  Speed the bubble moves to the top, between 1.5 - 6 seconds
             let speed = Math.floor(Math.random() * (6 - 1.5 + 1)) + 1;
-
-            // Random choice of 4 colors
-            // Colors: peachpuff, lemonchiffon, lightblue, lavender
+            // Colors in order w/ 57% transparency: peachpuff, lemonchiffon, lightblue, lavender
             let colors = ['#ffd9b893', '#fffacd93', '#add8e693', '#e6e6fa93'];
             let randomColor = colors[Math.floor(Math.random() * colors.length)];
-
-            //  Transform the scale as the bubbles approach top of screen
             let scales = [0.25, 0.5, 0.75, 1.0, 1.25, 1.5];
             let randomScale = scales[Math.floor(Math.random() * scales.length)];
-
-            //  Pixel size for width and height of bubble, between 25 - 125 in increments
             let sizes = [25, 50, 75, 100, 125];
             let randomSize = sizes[Math.floor(Math.random() * sizes.length)]
 
-            //  Give Bubble class values
+            //  Assign to Bubble class values
             Bubble.x = moveX;
             Bubble.delay = wait;
             Bubble.rate = speed;
@@ -77,7 +73,7 @@ class Multibubble {
     render() {
         for (let i = 0; i < this.bubbles.length; i++) {
             let div = document.createElement('div');
-            div.className = 'bubble'; 
+            div.classList = 'bubble'; 
             document.getElementById('main').appendChild(div);
             div.style.left = `${this.bubbles[i].x}%`;
             div.style.animationDuration = `${this.bubbles[i].rate}s`;
@@ -86,17 +82,35 @@ class Multibubble {
             div.style.transform = `translateY(-100%) scale(${this.bubbles[i].scale})`;
             div.style.width = `${this.bubbles[i].size}px`;
             div.style.height = `${this.bubbles[i].size}px`;
+
+            //Match new bubbles to existing class on other bubbles
+            const mainClass = document.getElementById('main').classList;
+            
+            if (mainClass == 'main bigger') {
+                div.classList.toggle('bigger');
+            };
+            if (mainClass == 'main goth') {
+                div.classList.toggle('goth');
+            };
+            if (mainClass == 'main disco') {
+                div.classList.toggle('disco');
+            };
+            if (mainClass == 'main hat') {
+                div.classList.toggle('hat');
+            };
         } 
     }      
 }
 
-/**
- * *------  EXTRABUBBLE ------
+/** -----------------------------------------------------------------------
+ * *-----------------------------  EXTRABUBBLE ----------------------------
+ * 
  * Extends the Multibubble class so the factory doesn't
  * repeat the previous values when adding on
  * more bubbles with the "MORE BUBBLES" button.
  * See note at bottom of doc for details.
- */
+ * 
+ *-------------------------------------------------------------------------*/
 
 class Extrabubble extends Multibubble {
     makeBubbleSize() {
@@ -117,21 +131,17 @@ const myBubbles = new Multibubble;
 *
 *=============================================================*/
 
-/**
- * TODO: FIX TOGGLES AND RESETS.
- * Need to remove previous classes once a new button is pushed (working currently)
- * BUT also remove own element when pushed twice.
- * Example: 
- *      Pushing "BIGGER BUBBLES" once makes the bubbles bigger,
- *      pushing the button again does not remove the class
- *      because of the reset functions.
- * 
- *      Removing the reset functons keeps adding classes to existing ones. 
- *      TODO: Try switch?
- */
+
+/*  ---------------------------------------
+ * *--------  GLOBAL VARIABLE ------
+    --------------------------------------- */
+
+const main = document.getElementById('main');
 
 
-//*--------  MORE BUBBLES button ------
+/*  ---------------------------------------
+ * *--------  MORE BUBBLES button ------
+    --------------------------------------- */
 
 const bubbleButton = document.getElementById('more_bubbles');
 
@@ -140,28 +150,14 @@ bubbleButton.addEventListener('click', () => {
 })
 
 
-//*------  RESET button to clear screen  ------
+/** ------------------------------
+ * *------  FASTER button ------
+    ------------------------------  */
 
-const resetButton = document.getElementById('reset');
-
-resetButton.addEventListener('click', () => {
-   let bubbleField = document.getElementsByClassName('bubble');
-   //   Iterate and remove each child
-   for (let i = bubbleField.length - 1; i >= 0; i--) {
-       let pop = bubbleField[i];
-       document.getElementById('main').removeChild(pop);
-   }
-})
-
-//*------  FASTER button ------
-
-const faster = document.getElementById('faster');
-
-    /**                 Event Listener: Iteration Notes
+    /** Event Listener: Iteration Notes
      * ------------------------------------------------------------------------
-     * @param speed         Iterate through style property to find 
-     *                      "animation-duration" time (string).
-     *                       
+     * @param speed         Iterate through style property to find "animation-duration" time (string). 
+     *                      
      * @param oldSpeed      Parse the string to find integer. Assigned here.                      
      *      
      *  Remove the current duration property and replace with the new one.
@@ -169,8 +165,13 @@ const faster = document.getElementById('faster');
      * @param newSpeed      Takes the current duration and subtracts half a second.
      * 
      *  Will decrease every click. Maximum speed set to 0.5s
+     *  Couldn't toggle class like the "goth" et al because the animation duration is different
+     *  for every bubble and needs to increase by 0.5s every single click.
      * -------------------------------------------------------------------------
      */
+
+
+const faster = document.getElementById('faster');
 
 faster.addEventListener('click', () => {
     //  Declare all appended children with class "bubble"
@@ -189,12 +190,13 @@ faster.addEventListener('click', () => {
 })
 
 
-/*//*------  SLOWER BUTTON ------
- *  Works exact same as "FASTER" button
+/*/--------------------------------------------
+//*------  SLOWER BUTTON ------
+    Works exact same as "FASTER" button
     except that it adds time to the duration
     instead of subtracting it.
     Max "slowness": 20 seconds.
- */
+ --------------------------------------------   */
 
 const slower = document.getElementById('slower');
 
@@ -213,88 +215,115 @@ slower.addEventListener('click', () => {
     }
 })
 
-/**
+/** --------------------------------
  ** ------   BIGGER BUTTON    ------
- */
+    --------------------------------    */
 
  const bigger = document.getElementById('bigger');
 
  bigger.addEventListener('click', () => {
-     resetBubble();
-     resetMain();
-     document.querySelectorAll('.bubble').forEach(bubble => {
-         bubble.classList.toggle('bigger');
-     });
-     document.getElementById('main').classList.toggle('bigger');
+    removeDisco();
+    removeGoth();
+    removeHat();
+    document.querySelectorAll('.bubble').forEach(bubble => {
+        bubble.classList.toggle('bigger');
+    });
+    main.classList.toggle('bigger');
  })
 
-/**
- ** ------   FANCY BUTTON    ------
- */
-const fancy = document.getElementById('fancy');
-
-fancy.addEventListener('click', () => {
-    resetBubble();
-    resetMain();
-    document.querySelectorAll('.bubble').forEach(bubble => {
-        // resetFancy();
-        bubble.classList.toggle('hat');
-    })
-})
-
-/**
+/** -------------------------------
  ** ------   GOTH BUTTON    ------
- */
+    ------------------------------- */
 
 const goth = document.getElementById('goth');
    
 goth.addEventListener('click', () => {
-    resetBubble();
-    resetMain();
+    removeDisco();
+    removeHat();
+    removeBigger();
     document.querySelectorAll('.bubble').forEach(bubble => {
         bubble.classList.toggle('goth');
     });
-    document.getElementById('main').classList.toggle('goth');
+    main.classList.toggle('goth');
 })
 
-/**
+/** --------------------------------
  ** ------   DISCO BUTTON    ------
- */
+    --------------------------------    */
 
 const disco = document.getElementById('disco');
 
 disco.addEventListener('click', () => {
-    resetBubble();
-    resetMain();
+    removeGoth();
+    removeHat();
+    removeBigger();
     document.querySelectorAll('.bubble').forEach(bubble => {
         bubble.classList.toggle('disco');
     });
-    document.getElementById('main').classList.toggle('disco');
+    main.classList.toggle('disco');
+})
+
+/** --------------------------------
+ ** ------   FANCY BUTTON    ------
+--------------------------------    */
+const fancy = document.getElementById('fancy');
+
+fancy.addEventListener('click', () => {
+    removeDisco();
+    removeGoth();
+    removeBigger();
+    document.querySelectorAll('.bubble').forEach(bubble => {
+        // resetFancy();
+        bubble.classList.toggle('hat');
+    })
+    main.classList.toggle('hat');
+})
+
+/** --------------------------------------------
+ * *------  RESET button to clear screen  ------
+    --------------------------------------------     */
+
+const resetButton = document.getElementById('reset');
+
+resetButton.addEventListener('click', () => {
+   let bubbleField = document.getElementsByClassName('bubble');
+   //   Iterate and remove each child
+   for (let i = bubbleField.length - 1; i >= 0; i--) {
+       let pop = bubbleField[i];
+       main.removeChild(pop);
+   }
 })
 
 
-
-/**
- * ------   Reset functions -------
- */
-
-function resetBubble () {
+function removeGoth () {
     document.querySelectorAll('.bubble').forEach(bubble => {
-        bubble.classList.remove('hat'); 
-        bubble.classList.remove('goth'); 
-        bubble.classList.remove('disco');  
-        bubble.classList.remove('bigger');
+    bubble.classList.remove('goth'); 
+    main.classList.remove('goth');
     }
 )}
 
-function resetMain () {
-    document.getElementById('main').classList.remove('bigger');
-    document.getElementById('main').classList.remove('hat');
-    document.getElementById('main').classList.remove('disco');
-    document.getElementById('main').classList.remove('goth');
-}
+function removeDisco () {
+    document.querySelectorAll('.bubble').forEach(bubble => {
+    bubble.classList.remove('disco'); 
+    main.classList.remove('disco');
+    }
+)}
 
+function removeHat () {
+    document.querySelectorAll('.bubble').forEach(bubble => {
+    bubble.classList.remove('hat'); 
+    main.classList.remove('hat');
+    }
+)}
 
+function removeBigger () {
+    document.querySelectorAll('.bubble').forEach(bubble => {
+    bubble.classList.remove('bigger'); 
+    main.classList.remove('bigger');
+    }
+)}
+
+//===================================================================================================
 /**     NOTE:
  * !    What's up with the EXTRABUBBLE class?
  * 
